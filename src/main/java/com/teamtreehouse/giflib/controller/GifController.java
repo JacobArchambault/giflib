ac5be797781c6d3e71ac835abbb87881bcb988fc
jacobarchambault <jacobarchambault@gmail.com>
2019-03-15 19:37:16 -0400

    Add content

---
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -0,0 +1,14 @@
+package com.teamtreehouse.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+    @RequestMapping("/")
+    public String ListGifs() {
+        return "home";
+
+    }
+}
